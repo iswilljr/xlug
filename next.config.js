b8655e236be7abb75d0ceacff99c557a768bf4de
iswilljr <iswilljr@gmail.com>
2022-11-06 18:24:01 -0500

    chore: reinitialize project with Next.js 13

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
