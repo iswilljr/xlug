026637655a5617d969e9dd228b7c788a09a37144
iswilljr <iswilljr@gmail.com>
2022-11-06 17:36:36 -0500

    chore: reset url-shortener project

--- utils/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const { SUPABASE_URL = "", SUPABASE_ANON_KEY = "" } = process.env;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
