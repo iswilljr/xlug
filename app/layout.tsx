a8ab021bc3b0f348f8251a22dda8092b348d9824
iswilljr <iswilljr@gmail.com>
2022-11-06 19:09:45 -0500

    feat: add inter font to tailwindcss config and layout app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import { font } from "utils/font";
 import "styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.variable}>
       <head />
       <body className="bg-black text-white">{children}</body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
