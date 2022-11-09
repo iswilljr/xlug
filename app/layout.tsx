64f1a37182542f92026a2a8e1756190521da43c1
iswilljr <iswilljr@gmail.com>
2022-11-08 19:20:05 -0500

    feat: add header to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import { font } from "utils/font";
 import "styles/globals.css";
+import Header from "components/header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={font.variable}>
       <head />
-      <body className="bg-black text-white">{children}</body>
+      <body className="bg-primary text-white text-custom pb-4">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { GithubIcon } from "./icons";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-40 w-full py-4 bg-primary duration-300">
+      <div className="flex container pl-4 pr-4 items-center justify-between mx-auto">
+        <Link href="/">
+          <div className="flex items-center cursor-pointer text-white hover:text-gray-300 transition-all">
+            <img src="/logo.png" alt="Logo" className="w-8 h-8" />
+            <h1 className="text-xl ml-2 mr-2">xlug</h1>
+          </div>
+        </Link>
+        <div className="flex items-center">
+          <a
+            href="https://github.com/iswilljr/xlug"
+            rel="noreferrer"
+            target="_blank"
+            className="hover:transform hover:scale-110 transition duration-200 ease-in-out"
+          >
+            <GithubIcon width={28} height={28} />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
