7144ae3384c782663c8b6eb29b283022f496cfd9
iswilljr <iswilljr@gmail.com>
2022-11-08 19:40:40 -0500

    feat: add 'new' page for create new xlugs

---
+++ app/new/page.tsx
@@ -0,0 +1,45 @@
+import Button from "components/button";
+import { RocketLaunchIcon } from "@heroicons/react/24/outline";
+
+export default function page() {
+  return (
+    <div className="container mx-auto px-4">
+      <form>
+        <div className="mb-5">
+          <label htmlFor="url">Enter the URL here:</label>
+          <input
+            id="url"
+            type="text"
+            placeholder="https://"
+            className="outline-none rounded-md px-4 py-2 w-full focus:border-none mt-1 bg-secondary text-white"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="xlug">Custom slug:</label>
+          <p className="text-gray-500">https://xlug.vercel.app/x/</p>
+          <div className="flex items-center justify-between mt-1">
+            <input
+              id="xlug"
+              type="text"
+              placeholder="Custom slug"
+              className="outline-none rounded-md px-4 py-2 w-full focus:border-none bg-secondary text-white"
+            />
+            <Button className="ml-2 bg-secondary">Random</Button>
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description">Description (optional):</label>
+          <textarea
+            id="description"
+            className="outline-none focus:border-white rounded-md px-4 py-2 w-full  mt-1 bg-secondary text-white"
+            // {...register("description")}
+          />
+        </div>
+        <Button type="submit" className="bg-secondary">
+          <RocketLaunchIcon className="mr-2 w-4 h-4" />
+          Create your link
+        </Button>
+      </form>
+    </div>
+  );
+}
