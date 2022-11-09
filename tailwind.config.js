b0a9b9d99ce2af344d35869d9b476d28f1fcd6e6
iswilljr <iswilljr@gmail.com>
2022-11-08 19:13:07 -0500

    feat: add colors palette to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-inter)", ...fontFamily.sans],
       },
+      fontSize: {
+        custom: "15px",
+      },
+      colors: {
+        primary: "#00060d",
+        secondary: "#040f1d",
+        light: "#11253e",
+        icon: "#fff",
+      },
     },
   },
   plugins: [],
