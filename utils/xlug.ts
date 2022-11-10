009fa5626e7ee0995d41a97960b131eb51932354
Will <iswilljr@gmail.com>
2022-11-10 22:45:22 +0000

    fix: sending supabase error messages to client

--- utils/xlug.ts
+++ utils/xlug.ts
@@ -4,7 +4,7 @@ import type { XlugProps as Xlug } from "components/xlug";
 export async function getXlug(xlug: string): Promise<Xlug | null> {
   const { data: xlugData, error } = await supabase.from("xlugs").select("*").eq("xlug", xlug);
 
-  if (error) throw Error(error.message);
+  if (error) throw Error("Something went wrong, please try agnin");
 
   return xlugData?.[0] ?? null;
 }
@@ -12,7 +12,10 @@ export async function getXlug(xlug: string): Promise<Xlug | null> {
 export async function createXlug(data: Partial<Xlug>): Promise<Xlug | null> {
   const { data: xlugData, error } = await supabase.from("xlugs").insert(data).select();
 
-  if (error) throw Error(error.message);
+  if (error)
+    throw Error(
+      error.code === "23505" ? "This xlug is alreadt taken, try another one" : "Something went wrong, please try agnin"
+    );
 
   return xlugData?.[0] ?? null;
 }
