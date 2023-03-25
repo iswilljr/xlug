5c09c163e3c48ec2ca8ef24c5d6c3a040f178ca7
iswilljr <iswilljr@gmail.com>
2023-03-25 14:21:51 -0500

    feat: dynamic import of client side components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,27 @@
+import dynamic from "next/dynamic";
 import { MantineProvider } from "@mantine/core";
-import { ModalsProvider } from "@mantine/modals";
 import { DefaultSeo } from "next-seo";
-import { Toaster } from "sonner";
 import { defaultSeo } from "@/next-seo.config";
 import { theme } from "@/utils/theme";
-import { NavigationProgress } from "@/components/NavigationProgress";
 import { Layout } from "@/components/Layout/Layout";
 import type { AppProps } from "next/app";
+
+const Toaster = dynamic(async () => (await import("sonner")).Toaster, { ssr: false });
+const NavigationProgress = dynamic(async () => (await import("@/components/NavigationProgress")).NavigationProgress, {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   const { layoutProps } = Component as any;
 
   return (
     <MantineProvider withNormalizeCSS withCSSVariables theme={theme}>
-      <ModalsProvider>
-        <Layout {...layoutProps}>
-          <DefaultSeo {...defaultSeo} />
-          <Component {...pageProps} />
-          <NavigationProgress />
-          <Toaster position="bottom-right" theme="dark" closeButton richColors />
-        </Layout>
-      </ModalsProvider>
+      <Layout {...layoutProps}>
+        <DefaultSeo {...defaultSeo} />
+        <Component {...pageProps} />
+        <NavigationProgress />
+        <Toaster position="bottom-right" theme="dark" closeButton richColors />
+      </Layout>
     </MantineProvider>
   );
 }
