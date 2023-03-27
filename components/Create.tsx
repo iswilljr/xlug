a14324888cf5f0043863fef6ef7cae74b0b6042f
iswilljr <iswilljr@gmail.com>
2023-03-27 16:13:06 -0500

    feat: new xlug page now creates the xlug

--- components/Create.tsx
+++ components/Create.tsx
@@ -1,19 +1,83 @@
+import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
+import { Box } from "@mantine/core";
+import { useSession } from "@supabase/auth-helpers-react";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
-import { Box } from "@mantine/core";
+import { type FormErrors, useForm, zodResolver } from "@mantine/form";
+import axios, { type Response } from "redaxios";
+import { toast } from "sonner";
+import type { ZodSchema } from "zod";
+import type { Database } from "@/types/supabase";
+
+const initialValues: Database["public"]["Tables"]["xlugs"]["Insert"] = {
+  xlug: "",
+  destination: "",
+  description: "",
+};
 
 interface CreateProps {
-  action?: string;
+  actionLabel: string;
+  action: string;
+  schema: ZodSchema<typeof initialValues>;
+  successMessage: string;
+  onUpdate: (res: Response<any>) => void;
 }
 
-export function Create({ action }: CreateProps) {
+const onError = (errors: FormErrors) => {
+  const key = Object.keys(errors)[0];
+  toast.error(errors[key]);
+};
+
+export function Create({ actionLabel, action, successMessage, schema, onUpdate }: CreateProps) {
+  const router = useRouter();
+  const session = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const { onSubmit, getInputProps } = useForm({
+    initialValues,
+    validate: zodResolver(schema),
+  });
+
+  const onValid = useCallback(
+    async (values: typeof initialValues) => {
+      if (submitting) return;
+      setSubmitting(true);
+
+      try {
+        const res = await axios.post(action, values);
+        if (!session) onUpdate(res);
+
+        toast.success(successMessage);
+        void router.push("/dashboard");
+      } catch (error: any) {
+        const message = typeof error.data === "string" ? error.data : error.data?.message;
+        toast.error(message ?? "Something went wrong, try again");
+      } finally {
+        setSubmitting(false);
+      }
+    },
+    [action, successMessage, onUpdate, router, session, submitting]
+  );
+
   return (
-    <Box component="form" py="md" sx={{ display: "grid", gap: 12 }}>
-      <Input id="destination" label="Destination URL" placeholder="https://xlug.vercel.app" inputMode="url" />
-      <Input id="id" label="Custom Id" placeholder="xlug" inputMode="url" />
-      <Input textarea id="description" label="Description (optional)" />
-      <Button px="xl" py={6} w="fit-content">
-        {action ?? "Create Xlug"}
+    <Box
+      aria-disabled={submitting}
+      onSubmit={onSubmit(onValid, onError)}
+      component="form"
+      py="md"
+      sx={{ display: "grid", gap: 12 }}
+    >
+      <Input id="id" label="Custom Id" placeholder="xlug" {...getInputProps("xlug")} />
+      <Input
+        id="destination"
+        label="Destination URL"
+        placeholder="https://xlug.vercel.app"
+        inputMode="url"
+        {...getInputProps("destination")}
+      />
+      <Input textarea id="description" label="Description (optional)" {...getInputProps("description")} />
+      <Button loading={submitting} disabled={submitting} h={32} type="submit" px="xl" py={6} w="fit-content">
+        {actionLabel}
       </Button>
     </Box>
   );
--- pages/new.tsx
+++ pages/new.tsx
@@ -1,5 +1,17 @@
 import { Create } from "@/components/Create";
+import { newXlugSchema } from "@/utils/schemas";
+import { useLocalStorage } from "@mantine/hooks";
 
 export default function New() {
-  return <Create />;
+  const [, setLocalXlugs] = useLocalStorage<any[]>({ key: "local_xlugs", defaultValue: [] });
+
+  return (
+    <Create
+      action="/api/xlug/new"
+      actionLabel="Create Xlug"
+      successMessage="Successfully created your xlug"
+      schema={newXlugSchema}
+      onUpdate={(res) => setLocalXlugs((prev) => [...prev, res.data])}
+    />
+  );
 }
