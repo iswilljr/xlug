37b0c21dda01c6c2d713fccdec4cb16f12712f39
iswilljr <iswilljr@gmail.com>
2023-03-27 19:59:41 -0500

    feat: add middleware to redirect to the xlug destination

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { type NextRequest, NextResponse } from "next/server";
+import type { Database } from "./types/supabase";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  try {
+    const xlug = req.nextUrl.pathname.split("/").pop();
+
+    const supabase = createMiddlewareSupabaseClient<Database>({ req, res });
+    const { data, error } = await supabase.from("xlugs").select("*").eq("xlug", xlug).single();
+
+    if (error) return res;
+
+    const redirectTo = new URL(data.destination);
+
+    return NextResponse.redirect(redirectTo);
+  } catch (error) {
+    return res;
+  }
+}
+
+export const config = {
+  matcher: "/x/:xlug*",
+};
