fb6f63626b9939f338cf3611458a05fafa5dd11f
iswilljr <iswilljr@gmail.com>
2023-07-30 16:41:18 -0500

    chore: remove old components and pages
    remove unused dependencies

--- src/types/mantine.d.ts
+++
@@ -1,9 +0,0 @@
-import type { MantineTheme } from '@mantine/core'
-
-declare module '@mantine/core' {
-  interface MantineThemeOther {
-    getBoxShadow: (theme: MantineTheme, color: string) => string
-  }
-}
-
-export {}
