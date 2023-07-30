abd643ceb143a74beb6c5c1201edf914fcbaa12c
iswilljr <iswilljr@gmail.com>
2023-07-29 19:23:48 -0500

    fix: not dynamic rendering in auth pages

--- src/app/(auth)/sign-in/layout.tsx
+++ src/app/(auth)/sign-in/layout.tsx
@@ -1,6 +1,8 @@
 import { redirect } from 'next/navigation'
 import { getServerSession } from '@/utils/auth'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Layout({ children }: React.PropsWithChildren) {
   const session = await getServerSession()
 
