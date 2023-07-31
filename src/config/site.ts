c9368973e0d99e8e1e98cb034613005b70f69a35
iswilljr <iswilljr@gmail.com>
2023-07-30 19:29:39 -0500

    chore: update site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,10 +1,11 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  title: 'Xlug',
-  description: 'A URL shortener built with NextJs.',
+  title: 'Xlug - Short and Manage your Long URLs.',
+  description: 'A link shortener tool that allows you to create shorter and more manageable versions of long URLs.',
   links: {
     twitter: 'https://twitter.com/iswilljr',
     github: 'https://github.com/iswilljr/xlug',
+    issues: 'https://github.com/iswilljr/xlug/issues/new',
   },
 }
