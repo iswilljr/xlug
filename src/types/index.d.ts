802b27d0f5f1a38fa7448550c1396011484032c7
iswilljr <iswilljr@gmail.com>
2023-08-01 13:26:51 -0500

    feat: remove unused file declaration file

--- src/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Database } from './supabase'
-
-export type Xlug = Database['public']['Tables']['xlugs']['Row']
-
-export type CreateXlug = Database['public']['Tables']['xlugs']['Insert' | 'Update']
