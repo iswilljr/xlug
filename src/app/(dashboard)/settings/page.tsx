5aeaf4af3bfe0646a7953da45ef05ecaf132118d
iswilljr <iswilljr@gmail.com>
2023-08-06 14:23:21 -0500

    chore: delete unused components and pages

--- src/app/(dashboard)/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardStackHeader } from '@/layout/dashboard/stack-header'
-
-export default function Page() {
-  return (
-    <>
-      <DashboardStackHeader title='Account Settings' />
-    </>
-  )
-}
--- src/utils/auth.ts
+++
@@ -1,9 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-
-export async function getServerSession() {
-  const supabase = createServerComponentClient({ cookies })
-  const response = await supabase.auth.getSession()
-
-  return response.data.session
-}
