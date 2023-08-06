5be56f3663be848dd20ec44d360501a856bf3e7c
iswilljr <iswilljr@gmail.com>
2023-08-06 14:19:37 -0500

    feat: add toaster to provide feedback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { inter } from '@/utils/fonts'
 import { generateBaseMetadata } from '@/utils/metadata'
 import { SessionProvider } from '@/providers/session'
+import { Toaster } from '@/providers/toaster'
 
 export const metadata = generateBaseMetadata()
 
@@ -9,7 +10,10 @@ export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          {children}
+          <Toaster />
+        </SessionProvider>
       </body>
     </html>
   )
