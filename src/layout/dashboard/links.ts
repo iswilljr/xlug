110fbf7edf1306d845f962706e8fda8cab226ad7
iswilljr <iswilljr@gmail.com>
2023-08-06 14:47:30 -0500

    feat: remove settings from avatar menu and submenu

--- src/layout/dashboard/links.ts
+++ src/layout/dashboard/links.ts
@@ -1,12 +1,12 @@
-import { AppWindow, HomeSimple, Settings } from 'iconoir-react'
+import { AppWindow, HomeSimple } from 'iconoir-react'
 
 export const avatarMenu = [
   { label: 'Home', href: '/', icon: HomeSimple },
   { label: 'Dashboard', href: '/dashboard', icon: AppWindow },
-  { label: 'Settings', href: '/Settings', icon: Settings },
+  // { label: 'Settings', href: '/Settings', icon: Settings },
 ]
 
 export const submenu = [
   { label: 'Links', href: '/dashboard' },
-  { label: 'Settings', href: '/settings' },
+  // { label: 'Settings', href: '/settings' },
 ]
